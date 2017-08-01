1811d270df981a7941d0b736ad556ed96ae018a6
wuhao4 <wuhao4@100tal.com>
2017-08-01 18:24:12 +0800

    add

--- conFusion/www/js/services.js
+++ conFusion/www/js/services.js
@@ -1,46 +1,44 @@
+/*jslint devel: true */
 'use strict';
 
 angular.module('conFusion.services', ['ngResource'])
-        .constant("baseURL","http://localhost:3000/")
-        .service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+        .constant("baseURL", "http://localhost:3000/")
+        .service('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
     
-                this.getDishes = function(){
-                    
-                    return $resource(baseURL+"dishes/:id",null,  {'update':{method:'PUT' }});
-                    
-                };
-    
-                // implement a function named getPromotion
-                // that returns a selected promotion.
-                this.getPromotion = function() {
-                    return   $resource(baseURL+"promotions/:id");;
-                }
-    
+        this.getDishes = function () {
                         
-        }])
+            return $resource(baseURL + "dishes/:id", null,  {'update': {method: 'PUT' }});
 
-        .factory('corporateFactory', ['$resource', 'baseURL', function($resource,baseURL) {
-    
-    
-            return $resource(baseURL+"leadership/:id");
-    
-        }])
+        };
 
-        .factory('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
-    
-    
-            return $resource(baseURL+"feedback/:id");
-    
-        }])
+                    // implement a function named getPromotion
+                    // that returns a selected promotion.
+        this.getPromotion = function () {
+            return $resource(baseURL + "promotions/:id");
+        };
 
-        .factory('favoriteFactory', ['$resource','baseURL',function($resource,baseURL) {
-            var favFac = {};
-            var favorites = [];
+
+    }])
+
+        .factory('corporateFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    
+        return $resource(baseURL + "leadership/:id");
+    
+    }])
+
+        .factory('feedbackFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    
+        return $resource(baseURL + "feedback/:id");
+    
+    }])
+
+        .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+        var favFac = {}, favorites = [];
             
-            favFac.addToFavorites = function (index) {
-                for (var i = 0; i < favorites.length;i++) {
-                    if (favorites[i].id == index)
-                        return;
+        favFac.addToFavorites = function (index) {
+            for (var i = 0; i < favorites.length; i++) {
+                if (favorites[i].id === index)
+                    return;
                 }
                 favorites.push({id: index});
             };
